bdac9ba8097c6f60349f53d5654c93cf2d6a6149
Mohanraj1100 <123471382+Mohanraj1100@users.noreply.github.com>
2023-02-21 18:30:24 +0530

    commit

--- src/main/java/exceptionshape/Shape.java
+++ src/main/java/exceptionshape/Shape.java
@@ -3,94 +3,101 @@ import java.util.*;
 import java.util.logging.*;
 public class Shape
 {
-    float length;
-    float breadth;
-    float height;
-    float width;
-    float base;
-    float side;
-    float radius;
-    float perimeter;
-    float area;
+    public static void main(String[] args) {
+    	int choice;
+        Logger log = Logger.getLogger("hi");
+        Scanner inputobj = new Scanner(System.in);
+        log.info("Which shape you want to calculate from below\n1.Rectangle\n2.Triangle\n3.Circle");
+        choice = inputobj.nextInt();
+        ShapeCalculation sc = new ShapeCalculation(choice);
+        sc.perimeterIs();
+        sc.areaIs();
+    }
+}
+class ShapeCalculation{
+	double length;
+    double breadth;
+    double height;
+    double width;
+    double base;
+    double side;
+    double radius;
+    double perimeter;
+    double area;
+    double pie=3.14;
+    double half=0.5;
     int choice;
     Logger log = Logger.getLogger("hi");
+    Scanner inputobj = new Scanner(System.in);
+    ShapeCalculation(int choice){
+    	this.choice = choice;
+    	switch (choice) {
+        case 1:
+        	try {
+            log.info("Enter Length");
+            length = inputobj.nextInt();
+            log.info("Enter Breadth");
+            breadth = inputobj.nextInt();
+        	}
+        	catch(InputMismatchException e)
+    		{
+    			log.info(String.valueOf(e));
+    			System.exit(0);
+    		}
+            break;
 
-    Shape() {
-        Scanner sc = new Scanner(System.in);
-        log.info("Which shape you want to calculate from below\n1.Rectangle\n2.Triangle\n3.Circle");
-        choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-		try{
-                log.info("Enter Length");
-                length = sc.nextInt();
-                log.info("Enter Breadth");
-                breadth = sc.nextInt();
-                perimeterIs();
-                areaIs();
-		}
-		catch(InputMismatchException e)
-		{
-			log.info(String.valueOf(e));
-			System.exit(0);
-		}
-		break;
+        case 2:
+        	try {
+            log.info("Enter Base");
+            base = inputobj.nextInt();
+            log.info("Enter height");
+            height = inputobj.nextInt();
+            log.info("Enter side");
+            side = inputobj.nextInt();
+        	}
+        	catch(InputMismatchException e)
+    		{
+    			log.info(String.valueOf(e));
+    			System.exit(0);
+    		}
+            break;
 
-            case 2:
-		try{
-                log.info("Enter Base");
-                base = sc.nextInt();
-                log.info("Enter height");
-                height = sc.nextInt();
-                log.info("Enter side");
-                side = sc.nextInt();
-                perimeterIs();
-                areaIs();
-		}
-		catch(InputMismatchException e)
-		{
-			log.info(String.valueOf(e));
-			System.exit(0);
-		}	
-                break;
+        case 3:
+        	try {
+            log.info("Enter radius");
+            radius = inputobj.nextInt();
+        	}
+        	catch(InputMismatchException e)
+    		{
+    			log.info(String.valueOf(e));
+    			System.exit(0);
+    		}
+            break;
 
-            case 3:
-		try{
-                log.info("Enter radius");
-                radius = sc.nextInt();
-                perimeterIs();
-                areaIs();
-		}
-		catch(InputMismatchException e)
-		{
-			log.info(String.valueOf(e));
-			System.exit(0);
-		}
-                break;
+        default:
+            log.info("You have entered invalid shapename ");
+            break;
 
-            default:
-                log.info("Entered invalid shape");
-                break;
-
-        }
-	sc.close();
     }
-
+    	
+    }
+    
     void perimeterIs() {
         switch (choice) {
             case 1:
                 perimeter = 2 * (length + breadth);
-                log.fine("Permeter of Rectangle is {}" ,perimeter);
+                log.log(Level.INFO,()->"Permeter of Rectangle is" +perimeter);
                 break;
 
             case 2:
                 perimeter = height + base + side;
-                log.fine("Perimeter of Triangle is {}",perimeter);
+                log.log(Level.INFO,()->"Permeter of Triangle is" +perimeter);
+                
                 break;
 
             case 3:
-                perimeter = 2 * 3.14 * radius;
-                log.fine("Perimeter of Circle is {} ",perimeter);
+                perimeter = 2 * pie * radius;
+                log.log(Level.INFO,()->"Permeter of Circle is" +perimeter);
                 break;
 		
 		default:
@@ -103,33 +110,22 @@ public class Shape
         switch (choice) {
             case 1:
                 area = length * breadth;
-                log.fine("Area of Rectangle is {}",area);
+                log.log(Level.INFO,()->"Area of Rectangle is"+area);
                 break;
 
             case 2:
-                area = .5 * (base * height);
-                log.fine("Area of Triangle is {} " ,area);
+                area = half * (base * height);
+                log.log(Level.INFO,()->"Area of Triangle is"+area);
                 break;
 
             case 3:
                 area = radius * radius;
-                log.fine("Area of Circle is {}",area);
+                log.log(Level.INFO,()->"Area of Circle is"+area);
                 break;
 		
             default:
             	log.info("Invalid");
             	break;
         }
-    }
-	
-    void display()
-    {
-	    log.info("Hence area and perimeter has been calculated");
-    }
-
-    public static void main(String[] args) {
-        Shape in = new Shape();
-	in.display();
-
     }
 }
